734789ffa49fdb6385b553345bdbb03470c11190
Divya kandpal <80275853+DivyaKandpal93@users.noreply.github.com>
2024-02-16 11:59:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function fibonacci(num) {
 // your code here
+	let fib = [0,1]
+	for (let index = 2; index < num; index++) {
+		fib[index]= fib[index-1] + fib[index-2];
+	}
+	console.log(fib);
+	return fib;
 }
 
 module.exports = fibonacci;
